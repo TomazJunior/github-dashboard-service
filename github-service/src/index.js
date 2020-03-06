e51a842d0f5bfb2421668a8c4169bdd6f09a74ad
Tomaz Júnior <tomaz.jr@gmail.com>
2020-03-06 23:46:09 +0000

    add pull request parameters

--- github-service/src/index.js
+++ github-service/src/index.js
@@ -20,10 +20,26 @@ exports.handler = async (event, context) => {
     console.log('github.service.handler.user.repos', 'process started');
     const { headers: { authorization } } = req;
     const { data } = await axios.get('https://api.github.com/user/repos', { headers: { authorization } });
-    return data.map(repo => {
-      const { id, name, full_name, html_url } = repo;
-      return { id, name, full_name, html_url };
+    const response = data.map(repo => {
+      const { id, name, full_name, html_url, owner: { login } } = repo;
+      return { id, name, full_name, html_url, owner: { login } };
     });
+    console.log('github.service.handler.user.repos', 'process completed');
+    return response;
+  });
+
+  api.get('/github/repos/:owner/:repo/pulls', async (req, res) => {
+    console.log('github.service.handler.repos.pulls', 'process started');
+    const { headers: { authorization } } = req;
+    const { owner, repo} = req.params;
+    const { state } = req.query;
+    const { data } = await axios.get(`https://api.github.com/repos/${owner}/${repo}/pulls?state=${state || 'all'}`, { headers: { authorization } });
+    const response = data.map(pr => {
+      const { id, title, state, url, html_url, locked, number, created_at, closed_at, merged_at, user: { login, avatar_url } } = pr;
+      return { id, title, state, url, html_url, locked, number, created_at, closed_at, merged_at, user: { login, avatar_url } };
+    });
+    console.log('github.service.handler.repos.pulls', 'process completed');
+    return response;
   });
 
   return api.run(event, context);
